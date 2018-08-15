82ecb4ea83067be7d06666a486e1ed40b67b89fb
Unknown <pk14116@gmail.com>
2018-08-16 00:57:14 +0800

    Revert "add GET method at /test"
    This reverts commit 8fb1628fa62d973e2b1dd3dee64105dd2a3f73d3.

--- app.py
+++ app.py
@@ -32,24 +32,10 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
-@app.route("/test", methods=['GET'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
