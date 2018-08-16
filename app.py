12cc8580c9c5f367738fd0cdd4f26cb57076596a
Unknown <pk14116@gmail.com>
2018-08-16 22:38:09 +0800

    add imgur info

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('FxI3Qlfn3Mwyne/OujcIsYfOQCcIOZTUIYbDF2d41/GZAlQv2p5FGkp/ategG6xe0ErAsJCQOeHxdSM1xJ7uCejar1IGM5tDCzSFp40QmWs0BEjVec2nkacPIrL8Hh8XVvBxUgEUsGG6U+nvyGRClgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('7c2930cb70180aae136f76504fba88bd')
-
+imgur_client_id = ef420e58e8af248
+imgur_client_secret = 461a057a65611590954d7692f78964920b484929	
+imgur_album_id = UxgXZbe
 
 @app.route("/callback", methods=['POST'])
 def callback():
