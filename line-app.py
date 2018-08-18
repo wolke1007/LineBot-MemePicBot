302b60c1558679522521a42e8095ad28e9a9ddfe
Unknown <pk14116@gmail.com>
2018-08-18 23:52:15 +0800

    add access token and refresh token

--- line-app.py
+++ line-app.py
@@ -2,7 +2,6 @@
 import random
 from flask import Flask, request, abort
 from imgurpython import ImgurClient
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,22 +9,22 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
 import tempfile, os
-
-line_channel_access_token = 'FxI3Qlfn3Mwyne/OujcIsYfOQCcIOZTUIYbDF2d41/GZAlQv2p5FGkp/ategG6xe0ErAsJCQOeHxdSM1xJ7uCejar1IGM5tDCzSFp40QmWs0BEjVec2nkacPIrL8Hh8XVvBxUgEUsGG6U+nvyGRClgdB04t89/1O/w1cDnyilFU='
-line_channel_secret = '7c2930cb70180aae136f76504fba88bd'
-client_id = 'ef420e58e8af248'
-client_secret = '461a057a65611590954d7692f78964920b484929'
-album_id = 'UxgXZbe'
-access_token = 
-refresh_token =
+from config import client_id, client_secret, album_id, access_token, refresh_token, line_channel_access_token, \
+    line_channel_secret
+# line_channel_access_token = 'FxI3Qlfn3Mwyne/OujcIsYfOQCcIOZTUIYbDF2d41/GZAlQv2p5FGkp/ategG6xe0ErAsJCQOeHxdSM1xJ7uCejar1IGM5tDCzSFp40QmWs0BEjVec2nkacPIrL8Hh8XVvBxUgEUsGG6U+nvyGRClgdB04t89/1O/w1cDnyilFU='
+# line_channel_secret = '7c2930cb70180aae136f76504fba88bd'
+# client_id = 'ef420e58e8af248'
+# client_secret = '461a057a65611590954d7692f78964920b484929'
+# album_id = 'UxgXZbe'
+# access_token = 'YOUR_IMGUR_ACCESS_TOKEN'
+# refresh_token = 'YOUR_IMGUR_ACCESS_TOKEN'
 
 # line_bot_api = LineBotApi()
 # handler = WebhookHandler('')
 # imgur_client_id = ef420e58e8af248
 # imgur_client_secret = 461a057a65611590954d7692f78964920b484929	
-# imgur_album_id = UxgXZbe
+imgur_album_id = 'UxgXZbe'
 app = Flask(__name__)
 line_bot_api = LineBotApi(line_channel_access_token)
 handler = WebhookHandler(line_channel_secret)
@@ -48,11 +47,9 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=(ImageMessage, TextMessage))
 def handle_message(event):
     if isinstance(event.message, ImageMessage):
-        print('enter image part') #Debug log
         ext = 'jpg'
         message_content = line_bot_api.get_message_content(event.message.id)
         # with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
@@ -60,20 +57,14 @@ def handle_message(event):
             for chunk in message_content.iter_content():
                 tf.write(chunk)
             tempfile_path = tf.name
-        print(os.listdir(./)) #Debug log
-        dist_path = tempfile_path + '.jpg'
+
+        dist_path = tempfile_path + '.' + ext
         dist_name = os.path.basename(dist_path)
         os.rename(tempfile_path, dist_path)
-        from imgurpython.helpers.error import ImgurClientError
         try:
-            # this part is from imgurpython Document
-            client = ImgurClient(client_id, client_secret)
-            authorization_url = client.get_auth_url('pin')
-            credentials = client.authorize('PIN OBTAINED FROM AUTHORIZATION', 'pin')
-            client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
-            # ######################################
+            client = ImgurClient(client_id, client_secret, access_token, refresh_token)
             config = {
-                'album': album_id,
+                'album': imgur_album_id,
                 'name': 'Catastrophe!',
                 'title': 'Catastrophe!',
                 'description': 'Cute kitten being cute on '
@@ -81,17 +72,12 @@ def handle_message(event):
             # path = os.path.join('static', 'tmp', dist_name)
             path = dist_name
             client.upload_from_path(path, config=config, anon=False)
-            print('before remove: '+os.listdir(./)) #Debug log
             os.remove(path)
-            print('after remove: '+os.listdir(./)) #Debug log
             print(path)
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='上傳成功'))
-        except ImgurClientError as e:
-            print(e.error_message)
-            print(e.status_code)
-        else:
+        except:
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='上傳失敗'))
@@ -120,7 +106,6 @@ def handle_message(event):
                     TextSendMessage(text=' yoyo'),
                     TextSendMessage(text='請傳一張圖片給我')
                 ])
-            print(os.listdir(./)) #Debug log
             return 0
 
 if __name__ == "__main__":
