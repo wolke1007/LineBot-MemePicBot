da9d6112e84958ac79fb1375388dfdd3690c582a
Unknown <pk14116@gmail.com>
2018-08-19 09:37:28 +0800

    fix indent error

--- app.py
+++ app.py
@@ -29,9 +29,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(line_channel_access_token)
 handler = WebhookHandler(line_channel_secret)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-
-
-        
     
     
 @app.route("/callback", methods=['POST'])
@@ -48,7 +45,6 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=(ImageMessage, TextMessage))
@@ -76,23 +72,23 @@ def handle_message(event):
 
     def UploadToImgur(Pic_Name='Default_Pic_Name'):
         try:
-        print('UploadToImgur Pic_Name: ' + Pic_Name)
-        client = ImgurClient(client_id, client_secret, access_token, refresh_token)
-        config = {
-            'album': imgur_album_id,
-            'name': 'Catastrophe!',
-            'title': 'Catastrophe!',
-            'description': 'Cute kitten being cute on '
-        }
-        path = os.path.join('static', 'tmp', dist_name)
-        print('path:'+path) #debug
-        client.upload_from_path(path, config=config, anon=False)
-        print(os.listdir(os.getcwd())) #debug
-        os.remove(path)  #debug
-        print(os.listdir(os.getcwd())) #debug
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='上傳成功'))
+            print('UploadToImgur Pic_Name: ' + Pic_Name)
+            client = ImgurClient(client_id, client_secret, access_token, refresh_token)
+            config = {
+                'album': imgur_album_id,
+                'name': 'Catastrophe!',
+                'title': 'Catastrophe!',
+                'description': 'Cute kitten being cute on '
+            }
+            path = os.path.join('static', 'tmp', dist_name)
+            print('path:'+path) #debug
+            client.upload_from_path(path, config=config, anon=False)
+            print(os.listdir(os.getcwd())) #debug
+            os.remove(path)  #debug
+            print(os.listdir(os.getcwd())) #debug
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='上傳成功'))
         except Exception as e:
             print(e)
             line_bot_api.reply_message(
