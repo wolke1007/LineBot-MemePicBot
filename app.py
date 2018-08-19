250f9db4324c75083a365117ce0e24d23b353a04
Unknown <pk14116@gmail.com>
2018-08-20 02:19:29 +0800

    test

--- app.py
+++ app.py
@@ -123,8 +123,8 @@ def handle_message(event):
             print(User_ID_Who_Set_Name) #debug
             print('User_ID_Who_Upload_Pic:') #debug
             print(User_ID_Who_Upload_Pic) #debug
-            Pic_Name = event.message.text[2:]
             global Pic_Name
+            Pic_Name = event.message.text[2:]
             print('128 Pic_Name: '+Pic_Name)
             line_bot_api.reply_message(
                 event.reply_token, [
