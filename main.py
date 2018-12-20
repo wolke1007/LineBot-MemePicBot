a0831b1c5ee891f20d388f08b775f34142f69cc0
cloudchen104 <cloud.chen@104.com.tw>
2018-12-20 15:47:34 +0800

    fix, depend text length to sort the list

--- main.py
+++ main.py
@@ -208,7 +208,8 @@ def CheckMsgContent(MsgContent):
             match_list.append(pic_name)
     # 先確認 match_list 有沒有東西
     if match_list:        
-        match_list.sort()
+        # key 這邊解決了如果不同名字，會依照字串長度排序
+        match_list.sort(key=lambda x: len(x))
         # 排序後取 match 字數最多的也就是右邊一個
         pic_name = match_list[-1]
         select_params_dict = {
