284e64013d494dc6eb5c207ac9474f9a55075759
cloudchen104 <cloud.chen@104.com.tw>
2019-02-01 16:14:47 +0800

    remove config related code

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 from os import getenv
-from os import getenv
 
 # imgur key
-# CLIENT_ID = "ef420e58e8af248"
-# CLIENT_SECRET = "1e0b2834e232b9ca67ccb3d6b2076d7160614e10"
-# ALBUM_ID = "UxgXZbe"
-# ACCESS_TOKEN = "f7256765b702f940b6f9124bd6a6d53b8d7b9c67"
-# REFRESH_TOKEN = "b9058ff301a411c2dcbef44ae5b5c6ca237d06c0"
 CLIENT_ID = getenv("CLIENT_ID")
 CLIENT_SECRET = getenv("CLIENT_SECRET")
 ALBUM_ID = getenv("ALBUM_ID")
@@ -14,11 +8,6 @@ ACCESS_TOKEN = getenv("ACCESS_TOKEN")
 REFRESH_TOKEN = getenv("REFRESH_TOKEN")
 
 # line bot key
-# LINE_CHANNEL_ACCESS_TOKEN = ("FxI3Qlfn3Mwyne/OujcIsYfOQCcIOZTUIYbDF2d41/"
-#                              "GZAlQv2p5FGkp/ategG6xe0ErAsJCQOeHxdSM1xJ7uCe"
-#                              "jar1IGM5tDCzSFp40QmWs0BEjVec2nkacPIrL8Hh8XVvB"
-#                              "xUgEUsGG6U+nvyGRClgdB04t89/1O/w1cDnyilFU=")
-# LINE_CHANNEL_SECRET = "7c2930cb70180aae136f76504fba88bd"
 LINE_CHANNEL_ACCESS_TOKEN = getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = getenv("LINE_CHANNEL_SECRET")
 
