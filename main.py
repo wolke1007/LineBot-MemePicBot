504f45823753b5e370dc4baff586be7e22f69de3
cloudchen104 <cloud.chen@104.com.tw>
2019-04-09 03:46:51 +0800

    match PEP8 rule

--- main.py
+++ main.py
@@ -170,18 +170,29 @@ def check_msg_content(msg_content, trigger_chat, group_id):
             group_id = group_id if group_id else 'NULL'
             if group_id is not 'NULL':
                 select_pre_sql = ("SELECT pic_link FROM pic_info "
-                                  "WHERE pic_name=:pic_name and group_id=:group_id")
-                res = dbm.select_from_db(select_pre_sql, params_dict={'pic_name': pic_name, 'group_id': group_id})
+                                  "WHERE pic_name=:pic_name "
+                                  "AND group_id=:group_id")
+                res = dbm.select_from_db(
+                    select_pre_sql, params_dict={
+                        'pic_name': pic_name, 'group_id': group_id})
                 print('check_msg_content group_id res:', group_id, res)
                 return res[0][0] if res else False
             else:
-                # 若 chat_mode = 1，group_id 會設定為 None 則邏輯會走到這裡，select 的時候就不能把 group_id 丟進去
-                select_pre_sql = ("SELECT pic_link FROM pic_info WHERE pic_name=:pic_name")
-                res = dbm.select_from_db(select_pre_sql, params_dict={'pic_name': pic_name})
+                # 若 chat_mode = 1，group_id 會設定為 None 則邏輯會走到這裡，select 的時候就不能把
+                # group_id 丟進去
+                select_pre_sql = (
+                    "SELECT pic_link FROM pic_info WHERE pic_name=:pic_name")
+                res = dbm.select_from_db(
+                    select_pre_sql, params_dict={
+                        'pic_name': pic_name})
                 from random import Random
                 random_index = Random()
                 random_index = random_index.choice(range(len(res)))
-                print('check_msg_content group_id res random_index res:', group_id, random_index, res)
+                print(
+                    'check_msg_content group_id res random_index res:',
+                    group_id,
+                    random_index,
+                    res)
                 return res[random_index][0] if res else False
         else:
             return False
@@ -201,8 +212,8 @@ def line_reply_msg(to, content, content_type):
 
 def line_push_text_msg(to, content):
     line_bot_api.push_message(
-            to,
-            TextSendMessage(text=content))
+        to,
+        TextSendMessage(text=content))
 
 
 # -------------------------------------------------
@@ -235,9 +246,15 @@ def handle_image(event):
     # 檢查該 user 是否已經被 banned
     if is_userid_banned(user_id):
         try:
-            raise Exception('This user id' + str(user_id) + 'got banned, refuse to do anything!')
+            raise Exception(
+                'This user id' +
+                str(user_id) +
+                'got banned, refuse to do anything!')
         except Exception:
-            logging.warning('This user id' + str(user_id) + 'got banned, refuse to do anything!')
+            logging.warning(
+                'This user id' +
+                str(user_id) +
+                'got banned, refuse to do anything!')
         return
     params_dict = {
         'user_id': user_id,
@@ -245,7 +262,9 @@ def handle_image(event):
     }
     # 名字設定好但還沒有 pic_link 的且 user_id 符合的就是準備要上傳的
     select_pre_sql = ("SELECT pic_name FROM pic_info "
-                      "WHERE pic_link IS NULL AND user_id=:user_id AND group_id=:group_id")
+                      "WHERE pic_link IS NULL "
+                      "AND user_id=:user_id "
+                      "AND group_id=:group_id")
     # 回傳為 list type 裡面包著 tuple 預期一定會拿到 pic_name 所以直接取第一個不怕噴錯
     pic_name = dbm.select_from_db(select_pre_sql, params_dict)
     pic_name = pic_name[0][0] if pic_name else None
@@ -283,19 +302,28 @@ def handle_text(event):
     add_userid_if_not_exist(user_id)  # 將 user 建檔管理
     if is_userid_banned(user_id):  # 檢查該 user 是否已經被 banned
         try:
-            raise Exception('This user id' + str(user_id) + 'got banned, refuse to do anything!')
+            raise Exception(
+                'This user id' +
+                str(user_id) +
+                'got banned, refuse to do anything!')
         except Exception:
-            logging.warning('This user id' + str(user_id) + 'got banned, refuse to do anything!')
+            logging.warning(
+                'This user id' +
+                str(user_id) +
+                'got banned, refuse to do anything!')
         return
 
     line_msg_text = event.message.text
     if isinstance(event.message, TextMessage):
         if event.message.text[0] == "#" and event.message.text[-1] == "#":
-            print('enter event.message.text[0] == "#" and event.message.text[-1] == "#"')  # debug
+            # debug
+            print(
+                'enter event.message.text[0] == "#" and event.message.text[-1] == "#"')
             # 因為會覆寫，所以直接再 Add 一次不用刪除，且統一用小寫儲存
             # 圖片名稱長度在此設定門檻，目前設定為 3~15 個字
             pic_name = line_msg_text[1:-1].lower()
-            if len(pic_name) >= PIC_NAME_LOW_LIMIT and len(pic_name) <= PIC_NAME_HIGH_LIMIT:
+            if len(pic_name) >= PIC_NAME_LOW_LIMIT and len(
+                    pic_name) <= PIC_NAME_HIGH_LIMIT:
                 if is_filename_exist(pic_name, group_id):
                     # 如果圖片重複了，對 user_id pic_link 欄位進行 update
                     print('圖片已經存在，更新 user_id pic_link')
@@ -321,11 +349,20 @@ def handle_text(event):
                     res = dbm.iud_from_db(insert_pre_sql, params_dict)
                     print('user_id pic_name 已經新增，準備接收新圖片')
                 if res is True:
-                    line_reply_msg(event.reply_token, '圖片名稱已設定完畢，請上傳圖片', content_type='text')
+                    line_reply_msg(
+                        event.reply_token,
+                        '圖片名稱已設定完畢，請上傳圖片',
+                        content_type='text')
                 else:
-                    line_reply_msg(event.reply_token, 'Database 寫檔失敗！請聯絡管理員', content_type='text')
+                    line_reply_msg(
+                        event.reply_token,
+                        'Database 寫檔失敗！請聯絡管理員',
+                        content_type='text')
             else:
-                line_reply_msg(event.reply_token, '圖片名稱長度需介於 3~10 個字（中英文或數字皆可)', content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    '圖片名稱長度需介於 3~10 個字（中英文或數字皆可)',
+                    content_type='text')
                 return
 
         elif event.message.text[:4] == "http" and (event.message.text[-4:] == ".jpg" or
@@ -339,13 +376,15 @@ def handle_text(event):
             # 名字設定好但還沒有 pic_link 的就是準備要上傳的
             # (只抓 user_id 符合的是為了避免設定名字後別人幫你上傳圖片的問題)
             select_pre_sql = ("SELECT pic_name FROM pic_info "
-                              "WHERE pic_link IS NULL AND user_id=:user_id AND group_id=:group_id")
+                              "WHERE pic_link IS NULL AND user_id=:user_id "
+                              "AND group_id=:group_id")
             # 回傳為 list type 裡面包著 tuple 預期一定會拿到 pic_name 所以直接取第一個不怕噴錯
             pic_name = dbm.select_from_db(select_pre_sql, params_dict)
             pic_name = pic_name[0][0] if pic_name else None
             if is_filename_exist(pic_name, group_id):
                 print('name already exist, start to upload')
-                pic_link, reply_msg = upload_to_imgur(pic_name, url=event.message.text)
+                pic_link, reply_msg = upload_to_imgur(
+                    pic_name, url=event.message.text)
                 params_dict = {
                     'user_id': user_id,
                     'pic_link': pic_link,
@@ -355,12 +394,18 @@ def handle_text(event):
                 update_pre_sql = ("UPDATE pic_info SET pic_link=:pic_link, group_id=:group_id "
                                   "WHERE user_id=:user_id AND pic_link IS NULL")
                 dbm.iud_from_db(update_pre_sql, params_dict)
-                line_reply_msg(event.reply_token, reply_msg, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_msg,
+                    content_type='text')
 
         elif event.message.text == "--help" or event.message.text == "-h":
             print('event.message.text == "--help or "-h"')  # debug
             help_content = HelpText.get_help_content()
-            line_reply_msg(event.reply_token, help_content, content_type='text')
+            line_reply_msg(
+                event.reply_token,
+                help_content,
+                content_type='text')
 
         elif event.message.text[:6] == "--mode" and group_id:
             print('event.message.text == "--mode"')  # debug
@@ -369,28 +414,45 @@ def handle_text(event):
                     = Mode.set_trigger_chat(event.message.text, group_id)
                 if update_pre_sql:
                     dbm.iud_from_db(update_pre_sql, params_dict)
-                line_reply_msg(event.reply_token, reply_content, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_content,
+                    content_type='text')
             elif event.message.text[7:-2] == "chat_mode":
                 params_dict, update_pre_sql, reply_content \
                     = Mode.set_chat_mode(event.message.text, group_id)
                 if update_pre_sql:
                     dbm.iud_from_db(update_pre_sql, params_dict)
-                line_reply_msg(event.reply_token, reply_content, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_content,
+                    content_type='text')
             else:
-                select_pre_sql = ("SELECT * FROM system WHERE group_id = :group_id")
-                system_config = dbm.select_from_db(select_pre_sql, params_dict={'group_id': group_id})
+                select_pre_sql = (
+                    "SELECT * FROM system WHERE group_id = :group_id")
+                system_config = dbm.select_from_db(
+                    select_pre_sql, params_dict={
+                        'group_id': group_id})
                 print('system_config', system_config)  # debug
                 reply_content = Mode.get_mode_status(system_config, group_id)
-                line_reply_msg(event.reply_token, reply_content, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_content,
+                    content_type='text')
 
         elif event.message.text == "--list":
             # 撈出除了 pic_name_list 這張圖片以外的所有圖片名稱
-            select_pre_sql = ("SELECT pic_name FROM pic_info WHERE pic_name != 'pic_name_list'")
+            select_pre_sql = (
+                "SELECT pic_name FROM pic_info WHERE pic_name != 'pic_name_list'")
             res = dbm.select_from_db(select_pre_sql, params_dict={})
             binary_pic = PicNameList.get_binary_pic(res)
-            pic_link, reply_msg = upload_to_imgur(pic_name='pic_name_list', binary_pic=binary_pic)
+            pic_link, reply_msg = upload_to_imgur(
+                pic_name='pic_name_list', binary_pic=binary_pic)
             if reply_msg != '上傳成功':
-                line_reply_msg(event.reply_token, reply_msg, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_msg,
+                    content_type='text')
             params_dict = {
                 'pic_link': pic_link,
                 'pic_name': 'pic_name_list'
@@ -408,29 +470,43 @@ def handle_text(event):
             print('delete pic_name:', pic_name)
             if pic_name:
                 reply_content = DeletePic.del_pic(pic_name, group_id)
-                line_reply_msg(event.reply_token, reply_content, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_content,
+                    content_type='text')
             else:
                 reply_content = "範例: --delete <檔案名稱>"
-                line_reply_msg(event.reply_token, reply_content, content_type='text')
+                line_reply_msg(
+                    event.reply_token,
+                    reply_content,
+                    content_type='text')
 # -------------------------------------------------
 # -                一般對話處理邏輯                 -
 # -------------------------------------------------
         else:
-            select_pre_sql = ("SELECT * FROM system WHERE group_id = :group_id")
-            system_config = dbm.select_from_db(select_pre_sql, params_dict={'group_id': group_id})
+            select_pre_sql = (
+                "SELECT * FROM system WHERE group_id = :group_id")
+            system_config = dbm.select_from_db(
+                select_pre_sql, params_dict={
+                    'group_id': group_id})
             print('system_config, group_id', system_config, group_id)
             if not system_config and group_id is not 'NULL':
                 print('該群組於System中還沒有資料，建立一筆資料')
-                # 如果還沒有 system_config 且有 group_id 那就創一個，只設定 group_id 其他用 default
-                insert_pre_sql = ("INSERT INTO system (group_id) values (:group_id)")
-                dbm.iud_from_db(insert_pre_sql, params_dict={'group_id': group_id})
+                # 如果還沒有 system_config 且有 group_id 那就創一個，只設定 group_id 其他用
+                # default
+                insert_pre_sql = (
+                    "INSERT INTO system (group_id) values (:group_id)")
+                dbm.iud_from_db(
+                    insert_pre_sql, params_dict={
+                        'group_id': group_id})
             else:
                 print('該群組於System中有資料了，或是是非群組對話')
                 group_id_list = [i[0] for i in system_config]
                 index = group_id_list.index(group_id)
                 print('group_id_list, index', group_id_list, index)
                 # system_config[index] 會回傳一個 tuple 類似像 ('Cxxxxxx', 1, 1, 3)
-                # 從左至右分別對應: group_id,	chat_mode, retrieve_pic_mode, trigger_chat
+                # 從左至右分別對應: group_id,     chat_mode, retrieve_pic_mode,
+                # trigger_chat
                 system_config = system_config[index]
                 print('system_config[index]', system_config)
 
@@ -446,15 +522,19 @@ def handle_text(event):
                 # 1 = 隨機回所有 group 創的圖(預設)
                 elif system_config[1] is 1:
                     print('chat_mode is 1')
-                    pic_link = check_msg_content(event.message.text, trigger_chat, group_id=None)
+                    pic_link = check_msg_content(
+                        event.message.text, trigger_chat, group_id=None)
                     if pic_link:
                         print('pic_link', pic_link)
-                        line_reply_msg(event.reply_token, pic_link, content_type='image')
+                        line_reply_msg(
+                            event.reply_token, pic_link, content_type='image')
                 # 2 = 只回該 group 創的圖
                 elif system_config[1] is 2:
                     # 搜尋時帶上 group_id 判斷是否符合同群組
                     print('chat_mode is 2, group_id:', group_id)
-                    pic_link = check_msg_content(event.message.text, trigger_chat, group_id=group_id)
+                    pic_link = check_msg_content(
+                        event.message.text, trigger_chat, group_id=group_id)
                     if pic_link:
                         print('pic_link', pic_link)
-                        line_reply_msg(event.reply_token, pic_link, content_type='image')
+                        line_reply_msg(
+                            event.reply_token, pic_link, content_type='image')
