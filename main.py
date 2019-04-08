a441f465f7633a98f4fd49031389d9f3217d0b6b
cloudchen104 <cloud.chen@104.com.tw>
2019-04-09 05:07:14 +0800

    fix, use dict before create

--- main.py
+++ main.py
@@ -315,90 +315,6 @@ def handle_text(event):
 
     line_msg_text = event.message.text
     if isinstance(event.message, TextMessage):
-        if event.message.text[0] == "#" and event.message.text[-1] == "#":
-            # debug
-            print(
-                'enter event.message.text[0] == "#" and event.message.text[-1] == "#"')
-            # 因為會覆寫，所以直接再 Add 一次不用刪除，且統一用小寫儲存
-            # 圖片名稱長度在此設定門檻，目前設定為 3~15 個字
-            pic_name = line_msg_text[1:-1].lower()
-            if len(pic_name) >= PIC_NAME_LOW_LIMIT and len(
-                    pic_name) <= PIC_NAME_HIGH_LIMIT:
-                if is_filename_exist(pic_name, group_id):
-                    # 如果圖片重複了，對 user_id pic_link 欄位進行 update
-                    print('圖片已經存在，更新 user_id pic_link')
-                    params_dict = {
-                        'user_id': user_id,
-                        'pic_name': pic_name,
-                        'group_id': group_id
-                    }
-                    update_pre_sql = ("UPDATE pic_info SET user_id=:user_id, pic_link=NULL "
-                                      "WHERE pic_name=:pic_name AND group_id=:group_id")
-                    res = dbm.iud_from_db(update_pre_sql, params_dict)
-                    print('user_id pic_link 已經淨空，準備接收新圖片')
-                else:
-                    # 如果沒重複直接 insert
-                    print('新增 user_id pic_name')
-                    params_dict = {
-                        'user_id': user_id,
-                        'pic_name': pic_name,
-                        'group_id': group_id
-                    }
-                    insert_pre_sql = ("INSERT INTO pic_info (user_id, pic_name, group_id)"
-                                      "values (:user_id, :pic_name, :group_id)")
-                    res = dbm.iud_from_db(insert_pre_sql, params_dict)
-                    print('user_id pic_name 已經新增，準備接收新圖片')
-                if res is True:
-                    line_reply_msg(
-                        event.reply_token,
-                        '圖片名稱已設定完畢，請上傳圖片',
-                        content_type='text')
-                else:
-                    line_reply_msg(
-                        event.reply_token,
-                        'Database 寫檔失敗！請聯絡管理員',
-                        content_type='text')
-            else:
-                line_reply_msg(
-                    event.reply_token,
-                    '圖片名稱長度需介於 3~10 個字（中英文或數字皆可)',
-                    content_type='text')
-                return
-
-        elif event.message.text[:4] == "http" and (event.message.text[-4:] == ".jpg" or
-                                                   event.message.text[-4:] == ".jpeg" or
-                                                   event.message.text[-4:] == ".png" or
-                                                   event.message.text[-4:] == ".gif"):
-            params_dict = {
-                'user_id': user_id,
-                'group_id': group_id
-            }
-            # 名字設定好但還沒有 pic_link 的就是準備要上傳的
-            # (只抓 user_id 符合的是為了避免設定名字後別人幫你上傳圖片的問題)
-            select_pre_sql = ("SELECT pic_name FROM pic_info "
-                              "WHERE pic_link IS NULL AND user_id=:user_id "
-                              "AND group_id=:group_id")
-            # 回傳為 list type 裡面包著 tuple 預期一定會拿到 pic_name 所以直接取第一個不怕噴錯
-            pic_name = dbm.select_from_db(select_pre_sql, params_dict)
-            pic_name = pic_name[0][0] if pic_name else None
-            if is_filename_exist(pic_name, group_id):
-                print('name already exist, start to upload')
-                pic_link, reply_msg = upload_to_imgur(
-                    pic_name, url=event.message.text)
-                params_dict = {
-                    'user_id': user_id,
-                    'pic_link': pic_link,
-                    'group_id': group_id
-                }
-                # 名字設定好但還沒有 pic_link 的且 user_id 符合的就是剛上傳好的
-                update_pre_sql = ("UPDATE pic_info SET pic_link=:pic_link, group_id=:group_id "
-                                  "WHERE user_id=:user_id AND pic_link IS NULL")
-                dbm.iud_from_db(update_pre_sql, params_dict)
-                line_reply_msg(
-                    event.reply_token,
-                    reply_msg,
-                    content_type='text')
-
         def ext_help():
             print('event.message.text == "--help or "-h"')  # debug
             help_content = HelpText.get_help_content()
@@ -484,6 +400,90 @@ def handle_text(event):
         ext_dict = {'--help':ext_help, '--mode':ext_mode,
                     '--list':ext_list, '--delete':ext_delete}
         # 使用 dict 將實作抽離至上方，方便觀察 if else 邏輯
+
+        if event.message.text[0] == "#" and event.message.text[-1] == "#":
+            # debug
+            print(
+                'enter event.message.text[0] == "#" and event.message.text[-1] == "#"')
+            # 因為會覆寫，所以直接再 Add 一次不用刪除，且統一用小寫儲存
+            # 圖片名稱長度在此設定門檻，目前設定為 3~15 個字
+            pic_name = line_msg_text[1:-1].lower()
+            if len(pic_name) >= PIC_NAME_LOW_LIMIT and len(
+                    pic_name) <= PIC_NAME_HIGH_LIMIT:
+                if is_filename_exist(pic_name, group_id):
+                    # 如果圖片重複了，對 user_id pic_link 欄位進行 update
+                    print('圖片已經存在，更新 user_id pic_link')
+                    params_dict = {
+                        'user_id': user_id,
+                        'pic_name': pic_name,
+                        'group_id': group_id
+                    }
+                    update_pre_sql = ("UPDATE pic_info SET user_id=:user_id, pic_link=NULL "
+                                      "WHERE pic_name=:pic_name AND group_id=:group_id")
+                    res = dbm.iud_from_db(update_pre_sql, params_dict)
+                    print('user_id pic_link 已經淨空，準備接收新圖片')
+                else:
+                    # 如果沒重複直接 insert
+                    print('新增 user_id pic_name')
+                    params_dict = {
+                        'user_id': user_id,
+                        'pic_name': pic_name,
+                        'group_id': group_id
+                    }
+                    insert_pre_sql = ("INSERT INTO pic_info (user_id, pic_name, group_id)"
+                                      "values (:user_id, :pic_name, :group_id)")
+                    res = dbm.iud_from_db(insert_pre_sql, params_dict)
+                    print('user_id pic_name 已經新增，準備接收新圖片')
+                if res is True:
+                    line_reply_msg(
+                        event.reply_token,
+                        '圖片名稱已設定完畢，請上傳圖片',
+                        content_type='text')
+                else:
+                    line_reply_msg(
+                        event.reply_token,
+                        'Database 寫檔失敗！請聯絡管理員',
+                        content_type='text')
+            else:
+                line_reply_msg(
+                    event.reply_token,
+                    '圖片名稱長度需介於 3~10 個字（中英文或數字皆可)',
+                    content_type='text')
+                return
+
+        elif event.message.text[:4] == "http" and (event.message.text[-4:] == ".jpg" or
+                                                   event.message.text[-4:] == ".jpeg" or
+                                                   event.message.text[-4:] == ".png" or
+                                                   event.message.text[-4:] == ".gif"):
+            params_dict = {
+                'user_id': user_id,
+                'group_id': group_id
+            }
+            # 名字設定好但還沒有 pic_link 的就是準備要上傳的
+            # (只抓 user_id 符合的是為了避免設定名字後別人幫你上傳圖片的問題)
+            select_pre_sql = ("SELECT pic_name FROM pic_info "
+                              "WHERE pic_link IS NULL AND user_id=:user_id "
+                              "AND group_id=:group_id")
+            # 回傳為 list type 裡面包著 tuple 預期一定會拿到 pic_name 所以直接取第一個不怕噴錯
+            pic_name = dbm.select_from_db(select_pre_sql, params_dict)
+            pic_name = pic_name[0][0] if pic_name else None
+            if is_filename_exist(pic_name, group_id):
+                print('name already exist, start to upload')
+                pic_link, reply_msg = upload_to_imgur(
+                    pic_name, url=event.message.text)
+                params_dict = {
+                    'user_id': user_id,
+                    'pic_link': pic_link,
+                    'group_id': group_id
+                }
+                # 名字設定好但還沒有 pic_link 的且 user_id 符合的就是剛上傳好的
+                update_pre_sql = ("UPDATE pic_info SET pic_link=:pic_link, group_id=:group_id "
+                                  "WHERE user_id=:user_id AND pic_link IS NULL")
+                dbm.iud_from_db(update_pre_sql, params_dict)
+                line_reply_msg(
+                    event.reply_token,
+                    reply_msg,
+                    content_type='text')
 
         elif event.message.text == "--help" or event.message.text == "-h":
             ext_dict.get('--help')()
